b5123c8ca119afd735619b5a446dd4983c6c6307
alexm92150 <145431029+alexm92150@users.noreply.github.com>
2023-09-22 11:51:09 +0200

    Add files via upload

---
+++ js/scriptrappel.js
@@ -0,0 +1,61 @@
+"use strict";
+
+//Quel type de variable ?
+
+let Avengers=10; //Le nombre d'Avengers (number)
+console.log (Avengers);
+
+let nom="César"; //Le nom de famille de l'empereur romain Jules (string)
+console.log (nom);
+
+let étoiles="inimaginable"; //Le nombre d'étoiles dans l'univers (string) mais en vrai (number)
+console.log (étoiles);
+
+let paillasson="Bienvenue"; //Message de bienvenue sur un paillasson (string)
+console.log (paillasson);
+
+let id=true; //Présence d'un face ID sur un téléphone (boolean)
+console.log (id);
+
+let carteGraphique=700; //Prix total d'un ordinateur monté à la main (concatenation de number)
+let processeur=600;
+let carteMere=400
+let ram=100;
+let boitier=50;
+let alim=100;
+
+let prix=carteGraphique+processeur+carteMere+ram+boitier+alim;
+console.log (prix);
+
+let nains="pilo," + " pilu," + " pilou," + " pila," + " pili," + " pilon," + " pile"; //noms des 7 nains (string)
+console.log (nains);
+// correction, tableau donc type objet
+
+
+let beaugoss="Sonic"; //profil tinder d'un beaugoss (string) ou boolean si réponse "vrai/faux"
+console.log (beaugoss);
+
+let clones=1000; //nombre de clone que peut faire Naruto (number)
+console.log (clones);
+
+let defauts="désorganisé," + " stressé," + " bavard"; //3 défauts à citer en entretient d'embauche (string)
+console.log (defauts);
+//correction, tableau donc type objet
+
+
+let flamme="Anne"; //prénom Anne dans la Flamme (string)
+console.log (flamme);
+
+let panier=256.99; //panier Amazon (number)
+console.log (panier);
+
+let fin=1453; //durée en années de la guerre de 100ans (concatenation de number)
+let debut=1337;
+let durée=fin-debut;
+console.log (durée);
+
+let bierre="alcoolisé"; //caractéristique d'une bière (string)
+console.log (bierre);
+
+let président="moi"; //prochain président de la république (string)
+console.log(président);
